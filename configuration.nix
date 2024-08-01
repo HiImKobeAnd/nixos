5e50ebfbf00892fd0f4555f0ffe4318f244ba8d4
hiimkobeand <hiimkobeand@gmail.com>
2024-08-01 13:04:36 +0200

    1

--- configuration.nix
+++ configuration.nix
@@ -31,7 +31,7 @@
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     git
-  #  wget
+    wget
   ];
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
--- rebuild.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-pushd ~/dotfiles/nixos/
-echo "NixOs Rebuilding ..."
-sudo nixos-rebuild switch &>nixos-switch.log || (
-	cat nixos-switch.log | grep --color error && false)
-gen=$(nixos-rebuild list-generations | grep current)
-git commit -am "$gen"
-popd
