8044869194d6f6b023b58a4d0e3e536cc90d1752
hiimkobeand <hiimkobeand@gmail.com>
2024-08-04 19:54:16 +0200

    14   2024-08-04 19:54:14   (current)

--- gnome.nix
+++ gnome.nix
@@ -3,6 +3,7 @@
 
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.gdm.wayland = false;
 
   services.xserver = {
     xkb.layout = "dk";
