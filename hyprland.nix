17b8f9862ddaa006caa8617a94bd51fc62add205
hiimkobeand <hiimkobeand@gmail.com>
2024-08-09 12:01:03 +0200

    84   2024-08-09 12:01:01   (current)

--- hyprland.nix
+++ hyprland.nix
@@ -1,13 +1,17 @@
 {pkgs, ...}: {
   programs.hyprland.enable = true;
-  programs.hyprlock.enable = true;
   environment.sessionVariables.NIXOS_OZONE_WL = "1"; # Hints to electron apps that wayland is in use.
+
+  services.greetd = {
+    enable = true;
+  };
 
   environment.systemPackages = with pkgs; [
     pyprland
     hyprpaper
     xwayland
 
+    kitty
     tofi
     alacritty
   ];
