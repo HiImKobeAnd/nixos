a405ddb8845c9780c25834d7416911a30d1b48cb
hiimkobeand <hiimkobeand@gmail.com>
2024-08-09 12:24:24 +0200

    86   2024-08-09 12:24:22   (current)

--- hyprland.nix
+++ hyprland.nix
@@ -2,10 +2,6 @@
   programs.hyprland.enable = true;
   environment.sessionVariables.NIXOS_OZONE_WL = "1"; # Hints to electron apps that wayland is in use.
 
-  services.greetd = {
-    enable = true;
-  };
-
   environment.systemPackages = with pkgs; [
     pyprland
     hyprpaper
