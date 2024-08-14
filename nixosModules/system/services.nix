94c8b4c98a734d16ec2f69da5e6d6059089f88c8
hiimkobeand <hiimkobeand@gmail.com>
2024-08-14 21:01:06 +0200

    164   2024-08-14 21:01:04   (current)

--- nixosModules/system/services.nix
+++ nixosModules/system/services.nix
@@ -4,9 +4,24 @@
   ];
   services.syncthing = {
     enable = true;
-    # overrideDevices = false;
-    #overrideFolders = false;
-    dataDir = "/home/hiimkobeand";
-    configDir = "/home/hiimkobeand/.config/syncthing";
+    overrideDevices = true;
+    overrideFolders = true;
+    #dataDir = "/home/hiimkobeand";
+    #configDir = "/home/hiimkobeand/.config/syncthing";
+
+    devices = {
+      "SM-S901B" = {id = "4HONYPG-EMW667X-XFYMDA5-MZRHJI2-AZFGS7N-UTYY2NX-J3TIFOJ-MGEUNA7";};
+    };
+
+    folders = {
+      "Documents" = {
+        path = "/home/hiimkobeand/Documents";
+        devices = ["SM-S901B"];
+      };
+      "Pictures" = {
+        path = "/home/hiimkobeand/Pictures";
+        devices = ["SM-S901B"];
+      };
+    };
   };
 }
