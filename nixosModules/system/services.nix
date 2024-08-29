11cc1c349f512c14aa1405dfa89acb62fd605ebe
hiimkobeand <hiimkobeand@gmail.com>
2024-08-29 14:49:22 +0200

    241   2024-08-29 14:49:20   (current)

--- nixosModules/system/services.nix
+++ nixosModules/system/services.nix
@@ -2,7 +2,6 @@
   environment.systemPackages = with pkgs; [
     syncthing
     spice
-    rnnoise-plugin
   ];
   services.syncthing = {
     enable = true;
@@ -17,39 +16,6 @@
   services.flatpak.enable = true;
 
   services.spice-vdagentd.enable = true;
-
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    extraConfig.pipewire = {
-      name = "libpipewire-module-filter-chain";
-      args = {
-        node.description = "Noise Canceling source";
-        media.name = "Noise Canceling source";
-        filter.graph = {
-          nodes = [
-            {
-              type = "ladspa";
-              name = "rnnoise";
-              plugin = "${pkgs.rnnoise-plugin}/lib/ladspa/librnnoise_ladspa.so";
-              label = "noise_suppressor_mono";
-              control = {"VAD Threshold (%)" = 50.0;};
-            }
-          ];
-        };
-        capture.props = {
-          node.name = "capture.rnnoise_source";
-          node.passive = true;
-        };
-        playback.props = {
-          node.name = "rnnoise_source";
-          media.class = "Audio/Source";
-        };
-      };
-    };
-  };
 
   services.keyd = {
     enable = true;
