e49e05ab3ac03f0fb86f0b2be55d38f10c3e9f33
hiimkobeand <hiimkobeand@gmail.com>
2024-09-20 14:50:23 +0200

    292   2024-09-20 14:50:10   (current)

--- nixosModules/desktop-environment/default.nix
+++ nixosModules/desktop-environment/default.nix
@@ -1,9 +1,9 @@
-{lib, ...}: {
+{...}: {
   imports = [
     ./gnome.nix
     ./cosmic-de.nix
   ];
 
-  gnome.enable = true;
-  cosmic-de.enable = false;
+  gnome.enable = false;
+  cosmic-de.enable = true;
 }
