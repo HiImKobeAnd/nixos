c9b2d454cf2517fd5655b2dfc0e64ed1ea3bbede
hiimkobeand <hiimkobeand@gmail.com>
2024-10-28 21:33:25 +0100

    380   2024-10-28 21:33:25   (current)

--- modules/application/misc.nix
+++ modules/application/misc.nix
@@ -9,24 +9,17 @@
     firefox
     inputs.zen-browser.packages."x86_64-linux".default
     thunderbird
-    bitwarden
     youtube-music
-    geary
     libreoffice
     zathura
-    sioyek
-    porsmo
     obsidian
     virt-manager
     qemu
     swtpm
     noisetorch
-    denaro
     gimp
-    komikku
     (chromium.override {enableWideVine = true;})
     widevine-cdm
     waydroid
-    suwayomi-server
   ];
 }
