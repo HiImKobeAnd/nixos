6f308a969e5786fa06c81f08ddba42f5b76a08d9
hiimkobeand <hiimkobeand@gmail.com>
2024-12-05 16:35:44 +0100

    435   2024-12-05 16:35:43   (current)

---
+++ update-nixos-1j.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+# Autoformat your nix files
+alejandra . >/dev/null
+
+# Shows your changes
+#git diff -U0 *.nix
+
+git add .
+
+echo "NixOS Rebuilding..."
+
+nh os switch ~/.dotfiles/nixos/ -- -j 1 --cores 4
+
+# Get current generation metadata
+current=$(sudo nix-env -p /nix/var/nix/profiles/system --list-generations | grep current)
+
+# Commit all changes witih the generation metadata
+git commit -am "$current"
