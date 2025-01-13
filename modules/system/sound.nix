84724bde62f85ddecb345f9a86155f6436f1df4b
hiimkobeand <hiimkobeand@gmail.com>
2025-01-13 13:33:58 +0100

    483   2025-01-13 13:33:58   (current)

--- modules/system/sound.nix
+++ modules/system/sound.nix
@@ -6,5 +6,13 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+    extraConfig.pipewire-pulse = {
+      pulse.cmd = [
+        {
+          cmd = "load-module";
+          args = "module-combine-sink";
+        }
+      ];
+    };
   };
 }
