906edafac9a88142846377710010753e1b36166f
hiimkobeand <hiimkobeand@gmail.com>
2025-01-17 13:57:11 +0100

    496   2025-01-17 13:57:10   (current)

--- modules/application/misc.nix
+++ modules/application/misc.nix
@@ -5,6 +5,7 @@
 }: {
   programs.noisetorch.enable = true;
   programs.kdeconnect.enable = true;
+  programs.dconf.enable = true;
   virtualisation.waydroid.enable = true;
   environment.systemPackages = with pkgs; [
     inputs.zen-browser.packages."x86_64-linux".default
