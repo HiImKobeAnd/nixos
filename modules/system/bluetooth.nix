b6eff6981e1bdc90d2ca2dcf07cf8827c81ce64e
hiimkobeand <hiimkobeand@gmail.com>
2025-02-01 14:11:49 +0100

    530   2025-02-01 14:11:48   (current)

--- modules/system/bluetooth.nix
+++ modules/system/bluetooth.nix
@@ -3,7 +3,7 @@
     enable = true;
     powerOnBoot = true;
     settings.General = {
-      Experimental = true;
+      Experimental = false;
     };
   };
   services.blueman.enable = false;
--- services/8bitdo.nix
+++ services/8bitdo.nix
@@ -1,13 +1,12 @@
 {pkgs, ...}: {
-  # Udev rules to start or stop systemd service when controller is connected or disconnected
-  hardware.xpadneo.enable = false;
-  # services.udev.extraRules = ''
-  #  # May vary depending on your controller model, find product id using 'lsusb'
-  #     SUBSYSTEM=="usb", ATTR{idVendor}=="2dc8", ATTR{idProduct}=="3106", ATTR{manufacturer}=="8BitDo", RUN+="${pkgs.systemd}/bin/systemctl start 8bitdo-ultimate-xinput@2dc8:3106"
-  #  # This device (2dc8:3016) is "connected" when the above device disconnects
-  #    SUBSYSTEM=="usb", ATTR{idVendor}=="2dc8", ATTR{idProduct}=="3016", ATTR{manufacturer}=="8BitDo", RUN+="${pkgs.systemd}/bin/systemctl stop 8bitdo-ultimate-xinput@2dc8:3106"
-  #'';
-  # services.udev.extraRules = ''
-  #  SUBSYSTEM=="usb", ATTR{idVendor}=="2dc8", ATTR{idProduct}=="3106", ATTR{manufacturer}=="8BitDo", RUN+="${pkgs.systemd}/bin/systemctl start 8bitdo-ultimate-xinput@2dc8:3106"
-  #'';
+  ### enable 8bitdo ultimate controller support
+  services.udev = {
+    extraRules = ''
+      ACTION=="add", \
+        ATTRS{idVendor}=="2dc8", \
+        ATTRS{idProduct}=="3106", \
+        RUN+="${pkgs.kmod}/bin/modprobe xpad", \
+        RUN+="${pkgs.bash}/bin/sh -c 'echo 2dc8 3106 > /sys/bus/usb/drivers/xpad/new_id'"
+    '';
+  };
 }
