5e6919c34bc12401f0252b4a1fc2b5489fd35a7e
hiimkobeand <hiimkobeand@gmail.com>
2025-02-21 22:26:05 +0100

    552   2025-02-21 22:26:04   (current)

--- modules/system/bluetooth.nix
+++ modules/system/bluetooth.nix
@@ -3,10 +3,7 @@
     enable = true;
     powerOnBoot = true;
     settings.General = {
-      Experimental = true;
+      Experimental = false;
     };
   };
-  environment.systemPackages = with pkgs; [
-    bluez
-  ];
 }
