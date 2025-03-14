a8832cd3c3d5d8afd807d840264c2b8a733287fb
hiimkobeand <hiimkobeand@gmail.com>
2025-03-14 15:46:12 +0100

    594   2025-03-14 15:46:12   (current)

--- homeModules/style.nix
+++ homeModules/style.nix
@@ -1,6 +1,6 @@
 {pkgs, ...}: {
   stylix = {
     enable = true;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
+    base16Scheme = "./gruvbox-dark-hard.yaml";
   };
 }
