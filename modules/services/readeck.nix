0c7183b94682143c3b7be783f6fd75f630955b9e
hiimkobeand <hiimkobeand@gmail.com>
2025-04-18 15:43:22 +0200

    13   2025-04-18 15:43:22   (current)

--- modules/services/readeck.nix
+++ modules/services/readeck.nix
@@ -1,6 +1,14 @@
-{ lib, config, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
 {
   config = lib.mkIf config.modules.services.readeck.enable {
+    environment.systemPackages = with pkgs; [
+      readeck
+    ];
     services.readeck = {
       enable = true;
       settings = {
