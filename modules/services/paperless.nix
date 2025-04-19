f2099cce937f5a9a9f6cbfa2fb6a92243dfbc0c3
hiimkobeand <hiimkobeand@gmail.com>
2025-04-19 14:13:06 +0200

    25   2025-04-19 14:13:05   (current)

--- modules/services/paperless.nix
+++ modules/services/paperless.nix
@@ -12,13 +12,6 @@
     services.paperless = {
       enable = true;
       port = 8240;
-      settings = {
-        PAPERLESS_OCR_LANGUAGE = "dan+eng";
-        PAPERLESS_OCR_USER_ARGS = {
-          optimize = 1;
-          pdfa_image_compression = "lossless";
-        };
-      };
     };
   };
 }
