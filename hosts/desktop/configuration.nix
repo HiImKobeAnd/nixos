4cf0cf7992d2deb33ceb2b8415e994eff5012cd1
hiimkobeand <hiimkobeand@gmail.com>
2025-04-22 15:16:38 +0200

    650   2025-04-22 15:16:37   (current)

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -5,56 +5,61 @@
     ../../modules
   ];
   # Setup
-  config.modules = {
-    desktop = "cosmic";
-    sound.enable = true;
-    nvidia.enable = true;
-    applications = {
-      essentials.enable = true;
-      games.enable = true;
-      librewolf.enable = true;
-      quickemu.enable = true;
-      waydroid.enable = true;
-      misc.enable = true;
+  config = {
+    modules = {
+      desktop = "cosmic";
+      sound.enable = true;
+      nvidia.enable = true;
+      applications = {
+        essentials.enable = true;
+        games.enable = true;
+        librewolf.enable = true;
+        quickemu.enable = true;
+        waydroid.enable = true;
+        misc.enable = true;
+      };
+      services = {
+        keyd.enable = true;
+        controllers.enable = true;
+        misc.enable = true;
+        syncthing.enable = true;
+      };
     };
-    services = {
-      keyd.enable = true;
-      controller.enable = true;
-      misc.enable = true;
-      syncthing.enable = true;
+
+    services.flatpak.enable = true;
+
+    # Bootloader
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
+
+    # Bluetooth
+    hardware.bluetooth.enable = true;
+
+    # Networking
+    networking.hostName = "nixos";
+    networking.networkmanager.enable = true;
+
+    # Nixpkgs
+    nixpkgs.config.allowUnfree = true;
+
+    # User
+    users.users.hiimkobeand = {
+      isNormalUser = true;
+      description = "Kobe Andersen";
+      extraGroups = [
+        "networkmanager"
+        "wheel"
+        "libvirtd"
+      ];
     };
-  };
-  services.flatpak.enable = true;
 
-  # Nixpkgs
-  nixpkgs.config.allowUnfree = true;
+    virtualisation.libvirtd.enable = true;
 
-  # Bluetooth
-  hardware.Bluetooth.enable = true;
-
-  # Bootloader
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  # Networking
-  networking.hostName = "nixos";
-  networking.networkmanager.enable = true;
-
-  # User
-  users.users.hiimkobeand = {
-    isNormalUser = true;
-    description = "Kobe Andersen";
-    extraGroups = [
-      "networkmanager"
-      "wheel"
-      "libvirtd"
+    nix.settings.experimental-features = [
+      "nix-command"
+      "flakes"
     ];
-  };
 
-  virtualisation.libvirtd.enable = true;
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
-  system.stateVersion = "24.05";
+    system.stateVersion = "24.05";
+  };
 }
