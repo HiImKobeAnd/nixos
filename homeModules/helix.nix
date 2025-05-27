19b4bf04ac2ad070a902a89bfe3b6a8290212de5
hiimkobeand <hiimkobeand@gmail.com>
2025-05-27 15:49:58 +0200

    690   2025-05-27 15:48:32   (current)

--- homeModules/helix.nix
+++ homeModules/helix.nix
@@ -13,11 +13,12 @@
     settings = {
       theme = "gruvbox";
       editor = {
+      auto-format = true;
         lsp.display-inlay-hints = true;
       };
     };
     extraPackages = with pkgs; [
-      rust-analyzer
+        rust-analyzer
       vscode-langservers-extracted
       markdown-oxide
       nixfmt-rfc-style
