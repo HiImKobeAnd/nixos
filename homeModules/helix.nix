46beb5b7ffe73f258d21f8723bad5aac339bf8e9
hiimkobeand <hiimkobeand@gmail.com>
2025-05-27 15:52:17 +0200

    691   2025-05-27 15:52:15   (current)

--- homeModules/helix.nix
+++ homeModules/helix.nix
@@ -8,17 +8,17 @@
         name = "nix";
         language-servers = [ "nixd" ];
         formatter.command = lib.getExe pkgs.nixfmt-rfc-style;
+    auto-format = true;
       }
     ];
     settings = {
       theme = "gruvbox";
       editor = {
-      auto-format = true;
         lsp.display-inlay-hints = true;
       };
     };
     extraPackages = with pkgs; [
-        rust-analyzer
+      rust-analyzer
       vscode-langservers-extracted
       markdown-oxide
       nixfmt-rfc-style
