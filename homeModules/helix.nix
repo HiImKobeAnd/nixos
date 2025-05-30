9971229c3f31524b553437aa73842c118c5400b2
hiimkobeand <hiimkobeand@gmail.com>
2025-05-30 14:07:48 +0200

    694   2025-05-30 14:07:48   (current)

--- homeModules/helix.nix
+++ homeModules/helix.nix
@@ -15,10 +15,20 @@
       theme = "gruvbox";
       editor = {
         lsp.display-inlay-hints = true;
+        cursor-shape = {
+          insert = "bar";
+        };
       };
     };
     extraPackages = with pkgs; [
       rust-analyzer
+      taplo
+      lldb
+      marksman
+      texlab
+      elixir-ls
+      typescript-language-server
+      docker-language-server
       vscode-langservers-extracted
       markdown-oxide
       nixfmt-rfc-style
