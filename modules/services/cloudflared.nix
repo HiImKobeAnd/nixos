7d10504f5c52f084eb655b36fb6d6d597a663188
hiimkobeand <hiimkobeand@gmail.com>
2025-06-07 18:33:14 +0200

    23   2025-06-06 19:04:27   (current)

--- modules/services/cloudflared.nix
+++ modules/services/cloudflared.nix
@@ -13,7 +13,7 @@
       enable = true;
       tunnels = {
         "b8fbe9aa-450b-4949-94b2-35bce8be1ca8" = {
-          credentialsFile = /home/server/.cloudflared/cert.pem;
+          credentialsFile = /home/server/.cloudflared/Homeserver.cred;
         };
       };
     };
