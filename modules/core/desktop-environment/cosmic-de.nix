557096023aa8e65d57193efc17bfc94b38455091
hiimkobeand <hiimkobeand@gmail.com>
2025-07-31 15:16:45 +0200

    733   2025-07-31 12:44:14   (current)

--- modules/core/desktop-environment/cosmic-de.nix
+++ modules/core/desktop-environment/cosmic-de.nix
@@ -12,6 +12,10 @@
     # ];
     services.displayManager.cosmic-greeter.enable = true;
     services.desktopManager.cosmic.enable = true;
+    # Fix for 90 sec wait when shutting down
+    services.geoclue2.enable = true;
+    services.geoclue2.enableDemoAgent = false;
+
     boot.kernelParams = [ "nvidia_drm.fbdev=1" ];
     environment.sessionVariables.COSMIC_DATA_CONTROL_ENABLED = 1;
     environment.systemPackages = with pkgs; [
