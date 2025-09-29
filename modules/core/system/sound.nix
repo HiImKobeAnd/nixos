9d41a6e5fe0f3954e4edd168ff82a1bfdd4c04d7
hiimkobeand <hiimkobeand@gmail.com>
2025-09-29 15:31:39 +0200

    787   2025-09-29 15:31:38   (current)

--- modules/core/system/sound.nix
+++ modules/core/system/sound.nix
@@ -8,31 +8,31 @@
       alsa.support32Bit = true;
       pulse.enable = true;
     };
-    services.pipewire.extraConfig.pipewire."50-combined_stream" = {
-      "context.modules" = [
-        {
-          name = "libpipewire-module-combine-stream";
-          args = {
-            "combine.mode" = "sink";
-            "node.name" = "combine_sink";
-            "node.description" = "Combined";
-            "combine.latency-compensate" = false;
-            "combine.props" = {
-              "audio.position" = [
-                "FL"
-                "FR"
-              ];
-            };
-            "stream.props" = { };
-            "stream.rules" = [
-              {
-                matches = [ { "media.class" = "Audio/Sink"; } ];
-                actions.create-stream = { };
-              }
-            ];
-          };
-        }
-      ];
-    };
+    # services.pipewire.extraConfig.pipewire."50-combined_stream" = {
+    #   "context.modules" = [
+    #     {
+    #       name = "libpipewire-module-combine-stream";
+    #       args = {
+    #         "combine.mode" = "sink";
+    #         "node.name" = "combine_sink";
+    #         "node.description" = "Combined";
+    #         "combine.latency-compensate" = false;
+    #         "combine.props" = {
+    #           "audio.position" = [
+    #             "FL"
+    #             "FR"
+    #           ];
+    #         };
+    #         "stream.props" = { };
+    #         "stream.rules" = [
+    #           {
+    #             matches = [ { "media.class" = "Audio/Sink"; } ];
+    #             actions.create-stream = { };
+    #           }
+    #         ];
+    #       };
+    #     }
+    #   ];
+    # };
   };
 }
