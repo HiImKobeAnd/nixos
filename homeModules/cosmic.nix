eee4991b7f036e9d6edbd57827a3818eac366ea6
hiimkobeand <hiimkobeand@gmail.com>
2025-12-17 22:46:52 +0100

    888   2025-12-17 22:46:52   (current)

--- homeModules/cosmic.nix
+++ homeModules/cosmic.nix
@@ -3,7 +3,6 @@
   imports = [
     inputs.cosmic-manager.homeManagerModules.cosmic-manager
   ];
-  programs.cosmic-manager.enable = true;
   wayland.desktopManager.cosmic = {
     enable = true;
     appearance.theme.mode = "dark";
--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -66,6 +66,9 @@
     networking.hostName = "hiimkobeand";
     networking.networkmanager.enable = true;
 
+    networking.useNetworkd = true;
+    networking.dhcpcd.enable = false;
+
     # Nixpkgs
     nixpkgs.config.allowUnfree = true;
 
