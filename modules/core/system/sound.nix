ce89e7ed290719726edbd586880076590ef09356
hiimkobeand <hiimkobeand@gmail.com>
2026-01-05 14:40:59 +0100

    892   2026-01-04 23:40:27   (current)

--- modules/core/system/sound.nix
+++ modules/core/system/sound.nix
@@ -2,11 +2,12 @@
 {
   config = lib.mkIf config.modules.sound.enable {
     security.rtkit.enable = true;
+    services.pipewire.audio.enable = true;
     services.pipewire = {
       enable = true;
-      # alsa.enable = true;
-      # alsa.support32Bit = true;
-      pulse.enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      # pulse.enable = true;
     };
     services.pipewire.extraConfig.pipewire."50-combined_stream" = {
       "context.modules" = [
