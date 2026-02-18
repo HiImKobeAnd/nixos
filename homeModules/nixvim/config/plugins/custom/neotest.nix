466387382a4bae94c429b611499e4ce0f5d855b1
hiimkobeand <hiimkobeand@gmail.com>
2026-02-18 13:38:16 +0100

    932   2026-02-18 13:38:16   (current)

--- homeModules/nixvim/config/plugins/custom/neotest.nix
+++ homeModules/nixvim/config/plugins/custom/neotest.nix
@@ -15,7 +15,7 @@
       summary.expandErrors = true;
       summary.follow = true;
       watch.enabled = true;
-      stategies = "integrated";
+      default_strategy = "integrated";
     };
     adapters = {
       elixir.enable = true;
@@ -28,26 +28,27 @@
     {
       mode = "n";
       key = "<leader>tt";
-      action.__raw = ''
-        function()
-          require('neotest').summary.toggle()
-        end
-      '';
-      options = {
-        desc = "[T]oggle [t]est summary";
-      };
+      action.__raw = "function() require('neotest').summary.toggle() end";
+      options.desc = "[T]oggle [t]est summary";
     }
     {
       mode = "n";
-      key = "<leader>r";
-      action.__raw = ''
-        function()
-        require('neotest').run.run()
-        end
-      '';
-      options = {
-        desc = "[R]un tests";
-      };
+      key = "<leader>tw";
+      action.__raw = "function() require('neotest').watch.toggle() end";
+      options.desc = "[T]oggle [w]atch mode";
+    }
+    # Test running
+    {
+      mode = "n";
+      key = "<leader>rr";
+      action.__raw = "function() require('neotest').run.run() end";
+      options.desc = "[R]un nearest test";
+    }
+    {
+      mode = "n";
+      key = "<leader>rt";
+      action.__raw = "function() require('neotest').run.run(vim.fn.getcwd()) end";
+      options.desc = "[R]un [t]est project";
     }
   ];
 }
