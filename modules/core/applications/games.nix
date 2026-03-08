607e87b47bfbe8fe164b522ea6d942739e572be6
hiimkobeand <hiimkobeand@gmail.com>
2026-03-08 10:19:23 +0100

    948   2026-03-08 10:19:23   (current)

--- modules/core/applications/games.nix
+++ modules/core/applications/games.nix
@@ -39,12 +39,12 @@
         enable = true;
         gamescopeSession.enable = true;
         remotePlay.openFirewall = true;
-        package = pkgs.steam.override {
-          extraEnv = {
-            GAMEMODERUN = "1";
-            PULSE_LATENCY_MSEC = "30";
-          };
-        };
+        # package = pkgs.steam.override {
+        #   extraEnv = {
+        #     GAMEMODERUN = "1";
+        #     PULSE_LATENCY_MSEC = "30";
+        #   };
+        # };
       };
       gamemode.enable = true;
       #honkers-railway-launcher.enable = true;
