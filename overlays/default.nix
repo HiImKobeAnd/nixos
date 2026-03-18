25818acaaf2d8abb6e05255e7eab0a5bb1aa73ba
hiimkobeand <hiimkobeand@gmail.com>
2026-03-18 16:00:49 +0100

    952   2026-03-18 16:00:49   (current)

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,16 +2,16 @@
 {
   nixpkgs.overlays = [
     (final: prev: {
-      gnomeExtensions = prev.gnomeExtensions // {
-        pop-shell = prev.gnomeExtensions.pop-shell.overrideAttrs (oldAttrs: {
-          src = final.fetchFromGitHub {
-            owner = "sasilanz";
-            repo = "shell";
-            rev = "fix-gnome-49-compatibility";
-            hash = "sha256-vgpw8INbzTiWfPmIXXYeBT12E4qFlUTeGEz1K/HPjZw=";
-          };
-        });
-      };
+      # gnomeExtensions = prev.gnomeExtensions // {
+      #   pop-shell = prev.gnomeExtensions.pop-shell.overrideAttrs (oldAttrs: {
+      #     src = final.fetchFromGitHub {
+      #       owner = "sasilanz";
+      #       repo = "shell";
+      #       rev = "fix-gnome-49-compatibility";
+      #       hash = "sha256-vgpw8INbzTiWfPmIXXYeBT12E4qFlUTeGEz1K/HPjZw=";
+      #     };
+      #   });
+      # };
     })
   ];
 }
