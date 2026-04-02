18f0d832dbe72cbb74b7284b9d9342cdad94e268
hiimkobeand <hiimkobeand@gmail.com>
2026-04-02 16:13:36 +0200

    961   2026-04-02 16:13:36   (current)

--- modules/services/screen-powerstate-services.nix
+++ modules/services/screen-powerstate-services.nix
@@ -6,68 +6,38 @@
 }:
 {
   config = lib.mkIf config.modules.services.screen-powerstate-services.enable {
-    # Turn monitor OFF on Shutdown/Reboot
-    systemd.services.monitor-off-shutdown = {
-      description = "Turn monitor off on shutdown";
+    systemd.services.ddc-monitor-off = {
+      description = "Turn off monitor before sleep/shutdown";
       before = [
-        "poweroff.target"
-        "reboot.target"
-        "halt.target"
+        "sleep.target"
+        "shutdown.target"
       ];
       wantedBy = [
-        "poweroff.target"
-        "reboot.target"
-        "halt.target"
+        "sleep.target"
+        "shutdown.target"
       ];
       serviceConfig = {
         Type = "oneshot";
-        ExecStart = "${pkgs.ddcutil}/bin/ddcutil setvcp d6 04"; # Run on shutdown
+        ExecStart = "-${pkgs.ddcutil}/bin/ddcutil setvcp d6 05";
+
+        TimeoutStopSec = "10s";
       };
     };
 
-    # Turn monitor ON at Boot
-    systemd.services.monitor-on-boot = {
-      description = "Turn monitor on at boot";
-      after = [
-        "display-manager.service"
-        "graphical.target"
-      ];
-      wantedBy = [ "graphical.target" ];
-
-      serviceConfig = {
-        Type = "oneshot";
-        ExecStartPre = "${pkgs.coreutils}/bin/sleep 2";
-        ExecStart = "${pkgs.ddcutil}/bin/ddcutil --noverify --sleep-multiplier 2 setvcp d6 01";
-      };
-    };
-
-    # Turn monitor OFF on Sleep
-    systemd.services.monitor-off-sleep = {
-      description = "Turn monitor off before sleep";
-      before = [ "sleep.target" ];
-      wantedBy = [ "sleep.target" ];
-      serviceConfig = {
-        Type = "oneshot";
-        ExecStart = "${pkgs.ddcutil}/bin/ddcutil setvcp d6 04";
-      };
-    };
-
-    # Turn monitor ON on Wake
-    systemd.services.monitor-on-wake = {
-      description = "Turn monitor on after wake";
+    systemd.services.ddc-monitor-on = {
+      description = "Turn on monitor after wake";
       after = [
         "suspend.target"
         "hibernate.target"
-        "hybrid-sleep.target"
       ];
       wantedBy = [
         "suspend.target"
         "hibernate.target"
-        "hybrid-sleep.target"
       ];
       serviceConfig = {
         Type = "oneshot";
-        ExecStart = "${pkgs.ddcutil}/bin/ddcutil setvcp d6 01";
+        ExecStartPre = "${pkgs.coreutils}/bin/sleep 1";
+        ExecStart = "-${pkgs.ddcutil}/bin/ddcutil setvcp d6 01";
       };
     };
   };
