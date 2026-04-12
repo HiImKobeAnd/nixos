f264cea2092510236687b9cd48f041a9b0c37abb
hiimkobeand <hiimkobeand@gmail.com>
2026-04-12 22:28:11 +0200

    1016   2026-04-12 22:28:11   (current)

--- config/modules/fish.nix
+++ config/modules/fish.nix
@@ -21,16 +21,7 @@
             set fish_prompt_pwd_dir_length 0
           '';
         };
-        bash = {
-          enable = true;
-          #   interactiveShellInit = ''
-          #     if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
-          #     then
-          #       shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
-          #       exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
-          #     fi
-          #   '';
-        };
+        bash.enable = true;
       };
     };
 }
