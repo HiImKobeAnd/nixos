ab5b6cdac5d4af119cf8f9aadfe842a9cdc00b05
hiimkobeand <hiimkobeand@gmail.com>
2026-04-21 10:44:32 +0200

    1028   2026-04-21 10:44:32   (current)

--- nixvim/config/plugins/custom/overseer.nix
+++ nixvim/config/plugins/custom/overseer.nix
@@ -54,7 +54,7 @@
     {
       mode = "n";
       key = "<leader>rr";
-      action.__raw = "function() require('overseer').list_tasks() end";
+      action.__raw = "function() require('overseer').run_task() end";
       options.desc = "[R]un task ove[r]view";
     }
     {
