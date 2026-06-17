89f9592fde4a1632eb4eb4ad7db5c9ca4179fa24
hiimkobeand <hiimkobeand@gmail.com>
2026-06-17 11:26:59 +0200

    1125   2026-06-17 11:13:11   (current)

--- config/modules/nixcord.nix
+++ config/modules/nixcord.nix
@@ -1,7 +1,13 @@
 { inputs, ... }:
 {
   flake.nixosModules.nixcord =
-    { pkgs, ... }:
+    { pkgs, lib, ... }:
+    let
+      vesktop-global-shortcuts = builtins.fetchTarball {
+        url = "https://github.com/Vencord/Vesktop/archive/refs/heads/global-shortcuts.tar.gz";
+        sha256 = "sha256:16f42bn8k4j2avm5rzr9lkmqrmp3b27b2l1nqp4mgmiy9pas1ry5";
+      };
+    in
     {
       imports = [
         inputs.nixcord.nixosModules.nixcord
@@ -10,6 +16,30 @@
         enable = true;
         discord.vencord.enable = true;
         vesktop.enable = true;
+
+        vesktop.package =
+          (pkgs.vesktop.override {
+            electron_40 = pkgs.electron_41;
+          }).overrideAttrs
+            (
+              oldAttrs:
+              let
+                src = vesktop-global-shortcuts;
+              in
+              {
+                inherit src;
+                pnpmDeps = pkgs.fetchPnpmDeps {
+                  pname = "vesktop";
+                  version = "1.6.5";
+                  inherit src;
+                  patches = [ ];
+                  pnpm = pkgs.pnpm_10_29_2;
+                  fetcherVersion = 3;
+                  hash = "sha256-TuFTXDrgLGJD0jaTeo66eHpHLjHKYofrZwn61aQLArY=";
+                };
+              }
+            );
+
         user = "hiimkobeand";
         config = {
           themeLinks = [
