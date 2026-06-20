f4d5bcce9057c9b59e2f9fa0d2078b0a188644b3
hiimkobeand <hiimkobeand@gmail.com>
2026-06-20 21:10:28 +0200

    23   2026-06-20 21:05:56   (current)

--- config/modules/nvf.nix
+++ config/modules/nvf.nix
@@ -2,115 +2,118 @@
   inputs,
   config,
   ...
-}: {
-  flake.nixosModules.nvf = {
-    pkgs,
-    dib,
-    ...
-  }: {
-    imports = [
-      inputs.nvf.nixosModules.default
-    ];
-    nix.nixPath = ["nixpkgs=${inputs.nixpkgs}"];
-    programs.nvf = {
-      enable = true;
-      settings = {
-        vim = {
-          options.tabstop = 4;
-          options.shiftwidth = 4;
-          searchCase = "smart";
-          viAlias = false;
-          vimAlias = true;
+}:
+{
+  flake.nixosModules.nvf =
+    {
+      pkgs,
+      dib,
+      ...
+    }:
+    {
+      imports = [
+        inputs.nvf.nixosModules.default
+      ];
+      nix.nixPath = [ "nixpkgs=${inputs.nixpkgs}" ];
+      programs.nvf = {
+        enable = true;
+        settings = {
+          vim = {
+            options.tabstop = 4;
+            options.shiftwidth = 4;
+            searchCase = "smart";
+            viAlias = false;
+            vimAlias = true;
 
-          #theme = {
-          #enable = true;
-          #name = "gruvbox";
-          #style = "dark";
-          #};
+            #theme = {
+            #enable = true;
+            #name = "gruvbox";
+            #style = "dark";
+            #};
 
-          autopairs.nvim-autopairs.enable = true;
+            autopairs.nvim-autopairs.enable = true;
 
-          binds = {
-            whichKey.enable = true;
-            hardtime-nvim.enable = true;
-          };
-
-          statusline = {
-            lualine.enable = true;
-          };
-
-          telescope.enable = true;
-
-          autocomplete = {
-            nvim-cmp.enable = true;
-          };
-
-          treesitter.enable = true;
-          lsp = {
-            enable = true;
-            formatOnSave = true;
-            inlayHints.enable = true;
-            lightbulb.enable = true;
-            lspconfig.enable = true;
-          };
-
-          visuals = {
-            nvim-web-devicons.enable = true;
-            fidget-nvim.enable = true;
-          };
-
-          notes.todo-comments.enable = true;
-          navigation.harpoon.enable = true;
-          #treesitter.context.enable = true;
-
-          ui = {
-            borders.enable = true;
-          };
-
-          diagnostics = {
-            nvim-lint.enable = true;
-          };
-
-          debugger = {
-            nvim-dap = {
-              enable = true;
-              ui.enable = true;
+            binds = {
+              whichKey.enable = true;
+              hardtime-nvim.enable = true;
             };
-          };
 
-          #spellcheck.enable = true;
-
-          languages = {
-            enableExtraDiagnostics = true;
-            enableFormat = true;
-            enableTreesitter = true;
-
-            nix = {
-              enable = true;
-              #format.type = [ "nixfmt" ];
-              #lsp.servers = [ "nixd" ];
+            statusline = {
+              lualine.enable = true;
             };
-            rust.enable = true;
-            fish.enable = true;
-            elixir.enable = true;
-            docker.enable = true;
-            bash.enable = true;
-            go.enable = true;
-            make.enable = true;
-            env.enable = true;
-            html.enable = true;
-            json.enable = true;
-            just.enable = true;
-            lua.enable = true;
-            markdown.enable = true;
-            python.enable = true;
-            sql.enable = true;
-            css.enable = true;
-            toml.enable = true;
-            typescript.enable = true;
+
+            telescope.enable = true;
+
+            autocomplete = {
+              nvim-cmp.enable = true;
+            };
+
+            treesitter.enable = true;
+            lsp = {
+              enable = true;
+              formatOnSave = true;
+              inlayHints.enable = true;
+              lightbulb.enable = true;
+              lspconfig.enable = true;
+            };
+
+            visuals = {
+              nvim-web-devicons.enable = true;
+              fidget-nvim.enable = true;
+            };
+
+            notes.todo-comments.enable = true;
+            navigation.harpoon.enable = true;
+            #treesitter.context.enable = true;
+
+            ui = {
+              borders.enable = true;
+            };
+
+            diagnostics = {
+              nvim-lint.enable = true;
+            };
+
+            debugger = {
+              nvim-dap = {
+                enable = true;
+                ui.enable = true;
+              };
+            };
+
+            #spellcheck.enable = true;
+
+            languages = {
+              enableExtraDiagnostics = true;
+              enableFormat = true;
+              enableTreesitter = true;
+
+              nix = {
+                enable = true;
+                #format.type = [ "nixfmt" ];
+                #lsp.servers = [ "nixd" ];
+              };
+              rust.enable = true;
+              fish.enable = true;
+              elixir.enable = true;
+              docker.enable = true;
+              bash.enable = true;
+              go.enable = true;
+              make.enable = true;
+              env.enable = true;
+              html.enable = true;
+              json.enable = true;
+              just.enable = true;
+              lua.enable = true;
+              markdown.enable = true;
+              python.enable = true;
+              sql.enable = true;
+              css.enable = true;
+              toml.enable = true;
+              typescript.enable = true;
+            };
           };
         };
       };
     };
-  };
 }
