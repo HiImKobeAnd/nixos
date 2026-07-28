60eb570a6892872ad352a9cb740c60989c3ee067
hiimkobeand <hiimkobeand@gmail.com>
2026-07-28 14:46:35 +0200

    1225   2026-07-28 14:45:02   (current)

--- config/modules/git.nix
+++ config/modules/git.nix
@@ -31,7 +31,7 @@
             navigate = true;
             side-by-side = true;
             line-numbers = true;
-            syntax-mapping = "*.luau:Lua";
+            syntax-mapping = "luau:Lua";
           };
           merge.conflictstyle = "zdiff3";
 
