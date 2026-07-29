7a81a77e63507e73f16108fb01022cb26c257456
hiimkobeand <hiimkobeand@gmail.com>
2026-07-29 21:59:19 +0200

    1233   2026-07-29 21:58:08   (current)

--- config/modules/nvf.nix
+++ config/modules/nvf.nix
@@ -24,7 +24,7 @@
 
           telescope.enable = true;
           treesitter.enable = true;
-          lazy.enable = true;
+          lazy.enable = false;
           autopairs.nvim-autopairs.enable = true;
 
           theme = {
