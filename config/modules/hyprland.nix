d845d485fc7dda47648ede672ee876fb95eccac4
hiimkobeand <hiimkobeand@gmail.com>
2026-08-01 14:23:58 +0200

    1254   2026-07-31 12:56:22   (current)

--- config/modules/hyprland.nix
+++ config/modules/hyprland.nix
@@ -19,6 +19,7 @@
         hyprcursor # For cursors
         rose-pine-hyprcursor # For cursors
         pulseaudio # For Noctalia plugin: Audio Switcher
+        linux-wallpaperengine # For Noctalia plugin: W engine
       ];
       programs.hyprland = {
         enable = true;
