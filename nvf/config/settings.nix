7f68751cef238a805bca78e234ade1f4f0263c18
hiimkobeand <hiimkobeand@gmail.com>
2026-08-15 11:32:25 +0200

    1276   2026-08-15 11:31:16   (current)

--- nvf/config/settings.nix
+++ nvf/config/settings.nix
@@ -39,11 +39,10 @@
 
     snippets.luasnip.enable = true;
     autocomplete.blink-cmp.enable = true;
-    # autocomplete.blink-cmp.mappings.confirm = "<C-y>";
-    # autocomplete.blink-cmp.mappings.close = "<C-e>";
-    # autocomplete.blink-cmp.mappings.next = "tab";
-    # autocomplete.blink-cmp.mappings.previous = "<s-tab";
-    autocomplete.blink-cmp.setupOpts.cmdline.keymap.preset = "default";
+    autocomplete.blink-cmp.mappings.confirm = "<C-y>";
+    autocomplete.blink-cmp.mappings.close = "<C-e>";
+    autocomplete.blink-cmp.mappings.next = "tab";
+    autocomplete.blink-cmp.mappings.previous = "<s-tab";
 
     session.nvim-session-manager = {
       enable = true;
