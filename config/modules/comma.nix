0be292d158454f4f59d0a9785095338cbf6ca696
hiimkobeand <hiimkobeand@gmail.com>
2026-09-12 21:16:51 +0200

    1321   2026-09-12 20:08:11   (current)

--- config/modules/comma.nix
+++ config/modules/comma.nix
@@ -1,11 +1,12 @@
 { inputs, ... }:
 {
   flake.nixosModules.comma =
-    { ... }:
+    { pkgs, ... }:
     {
       imports = [
         inputs.nix-index-database.nixosModules.default
       ];
       programs.nix-index-database.comma.enable = true;
+      programs.nix-index.package = inputs.nix-index-database.packages.${pkgs.stdenv.hostPlatform.system}.nix-index-with-small-db;
     };
 }
