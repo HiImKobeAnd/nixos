f3a9dea3b7c6d7f244bf003ccfb55e586e0aaac9
hiimkobeand <hiimkobeand@gmail.com>
2026-09-20 19:57:50 +0200

    1335   2026-09-20 19:56:06   (current)

--- nvf/config/settings.nix
+++ nvf/config/settings.nix
@@ -5,7 +5,6 @@
 }:
 {
   vim = {
-    extraPackages = [ pkgs.catimg ];
     viAlias = true;
     vimAlias = false;
 
@@ -175,42 +174,15 @@
       setupOpts = {
         defaults = {
           color_devicons = true;
-          preview.mime_hook = {
-            __raw = ''
-              function(filepath, bufnr, opts)
-                local is_image = function(filepath)
-                  local image_extensions = {'png', 'jpg', 'jpeg', 'webp'}
-                  local split_path = vim.split(filepath:lower(), '.', {plain=true})
-                  local extension = split_path[#split_path]
-                  return vim.tbl_contains(image_extensions, extension)
-                end
-
-                if is_image(filepath) then
-                  local term = vim.api.nvim_open_term(bufnr, {})
-                  local function send_output(_, data, _)
-                    for _, d in ipairs(data) do
-                      vim.api.nvim_chan_send(term, d .. "\r\n")
-                    end
-                  end
-                  vim.fn.jobstart(
-                    { 'catimg', filepath },
-                    { on_stdout = send_output, stdout_buffered = true, pty = true }
-                  )
-                else
-                  require("telescope.previewers.utils").set_preview_message(bufnr, opts.winid, "Binary cannot be previewed")
-                end
-              end
-            '';
+          pickers = {
+            find_files.find_command = [
+              "rg"
+              "--files"
+              "--hidden"
+              "--glob"
+              "!**/.git/*"
+            ];
           };
-        };
-        pickers = {
-          find_files.find_command = [
-            "rg"
-            "--files"
-            "--hidden"
-            "--glob"
-            "!**/.git/*"
-          ];
         };
       };
       mappings = {
